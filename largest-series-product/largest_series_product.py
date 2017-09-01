899e05ff05a94e56636b4c4ad28b2308737807c3
Rick B <rickblanco3@gmail.com>
2017-09-01 19:19:11 -0400

    Completed Largest Series Product, edited gitignore

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,2 +1,16 @@
-def largest_product():
-    pass
+from operator import mul
+def largest_product(n,series_size):
+    if series_size == 0:
+        return 1
+    
+    if len(n) < series_size or series_size < 0 or not n.isdigit():
+        raise ValueError
+
+    max_product = 0
+    series = (n[i:i+series_size] for i in range(len(n) - series_size + 1))
+    for s in series:
+        #print "current series: %s" % s
+        #print "multiple of current series: %d" % reduce(mul, map(int, list(s)))
+        max_product = max( max_product, reduce(mul, map(int, list(s))) )
+
+    return max_product
