d0a7e6e32e03ebcd3ee9d79184ab3e2f1caed18a
Rick B <rickblanco3@gmail.com>
2017-09-03 10:50:26 -0400

    Completed Grade School

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,3 +1,24 @@
 class School(object):
-    def __init__(self):
-        pass
+    def __init__(self,schoolname):
+        self._schoolname = schoolname
+        self._student_roll = {} #dict of type {grade_num:[student_a,student_b,...]}
+
+    def grade(self,grade_num):
+        '''
+        returns a tuple with all students in a given grade.
+        if no students in a given grade, return empty list
+        '''
+        return self._student_roll.get(grade_num, [])
+
+
+    def add(self,student_name,grade_num):
+        '''
+        adds a student to a grade
+        '''
+        self._student_roll.setdefault(grade_num,[]).append(student_name)
+
+    def sort(self):
+        '''
+        returns list of tuples containing all students, sorted by grade and name.
+        '''
+        return [(grade_num,tuple(sorted(self._student_roll[grade_num]))) for grade_num in sorted(self._student_roll)]
