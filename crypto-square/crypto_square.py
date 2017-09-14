05245020c827882f0b97bef0bb48fc46af791925
Rick B <rickblanco3@gmail.com>
2017-09-14 10:26:38 -0400

    Crypto Square: replacing range() with slice notation

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -5,7 +5,7 @@ def encode(plain):
     
     return ' '.join(
             [  # fetch the characters in normalized string that correspond to column i
-            ''.join([normalized[j] for j in range(i,len(normalized),cols)]) 
+                ''.join(normalized[i::cols]) 
                 for i in range(cols)
             ]
         )      
