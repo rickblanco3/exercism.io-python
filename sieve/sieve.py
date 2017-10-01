e2e13361196d614b6f4c271ef34669ce00b11e7f
Rick B <rickblanco3@gmail.com>
2017-10-01 13:06:30 -0400

    Using pylint on sieve.py

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,13 +1,15 @@
-from math import sqrt
+'''
+uses Sieve of Erathosthenes to
+find all prime numbers up to a given number
+'''
 
-def sieve(n):
-    '''Python implementation of Wikipedia basic example algorithm'''
-
-    A = {m : True for m in range(2,n+1)}
-
-    for i in range(2, int(sqrt(n))+1 ):
-        if A[i]:
-            for j in range(i**2, n+1, i):
-                A[j] = False
-
-    return [k for k in A.keys() if A[k] == True]
+def sieve(limit):
+    ''' python implementation of
+        Wikipedia basic example algorithm
+    '''
+    num_list = {m : True for m in range(2, limit+1)}
+    for i in range(2, int(limit**.5)+1):
+        if num_list[i]:
+            for j in range(i**2, limit+1, i):
+                num_list[j] = False
+    return filter(lambda k: num_list[k], num_list)
